8af1bbfcfbb1d1c304b0958147fb10ba29dae1e9
edison234456 <edleo486@gmail.com>
2016-04-25 19:46:09 -0500

    Added files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Areas.Startup))]
+namespace Areas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
