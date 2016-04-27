c5d8bc0e7af440fb0dedb4659020c1c871609ebb
davidRiivera <ing.driveraa@outlook.com>
2016-04-26 19:38:42 -0500

    Version de prueba

---
+++ Suma/Suma/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: David
+ * Date: 26/04/2016
+ * Time: 07:36 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Suma
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
